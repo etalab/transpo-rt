1bc8423903ce491d72ff373da4531abc852fce9e
antoine-de <antoine.desbordes@gmail.com>
2018-11-21 14:50:28 +0100

    slightly better errors

--- src/gtfs_rt.rs
+++ src/gtfs_rt.rs
@@ -1,21 +1,19 @@
 use actix_web::http::{ContentEncoding, StatusCode};
-use actix_web::{HttpRequest, HttpResponse, Result};
+use actix_web::{HttpRequest, HttpResponse, Result, error};
 use reqwest;
 use std::io::Read;
 
 pub fn gtfs_rt(_req: &HttpRequest) -> Result<HttpResponse> {
     let url = std::env::var("URL").expect("cannot find env var URL");
 
-    let mut pbf = reqwest::get(url.as_str())
-        .unwrap()
+    let pbf = reqwest::get(url.as_str())
+        .map_err(|e| error::ErrorInternalServerError(e))?
         .error_for_status()
-        .unwrap();
-
-    let mut buffer = vec![];
-    pbf.read_to_end(&mut buffer).unwrap();
-
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+        
+    let buffer: Result<Vec<u8>, _> = pbf.bytes().collect();
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("application/x-protobuf")
         .content_encoding(ContentEncoding::Identity)
-        .body(buffer))
+        .body(buffer?))
 }
