4f0773befb556956987432fd98115c75b91320db
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-01-11 16:56:06 +0100

    Handle non-available theoretical data for more endpoints

--- src/routes/stoppoints_discovery.rs
+++ src/routes/stoppoints_discovery.rs
@@ -85,18 +85,18 @@ pub async fn stoppoints_discovery_query(
     web::Query(query): web::Query<Params>,
     dataset_actor: web::Data<Addr<DatasetActor>>,
 ) -> actix_web::Result<web::Json<SiriResponse>> {
-    let dataset = dataset_actor
+    let result = dataset_actor
         .send(GetDataset)
         .await
         .map_err(|e| {
             log::error!("error while querying actor for data: {:?}", e);
             actix_web::error::ErrorInternalServerError("impossible to get data".to_string())
-        })?
-        .map_err(|e| {
-            actix_web::error::ErrorInternalServerError(
-                "theoretical dataset temporarily unavailable".to_string(),
-            )
         })?;
+
+    let dataset = match &(*result) {
+        Ok(dataset) => dataset,
+        Err(e) => return Err(actix_web::error::ErrorBadGateway("theoretical dataset temporarily unavailable".to_string()))
+    };
 
     Ok(web::Json(filter(&dataset, query)))
 }
